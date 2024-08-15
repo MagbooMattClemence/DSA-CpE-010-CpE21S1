67308fe1e8da64b2c2d86acbec24e634afe2afe7
Matt Clemence C. Magboo <qmccmagboo@tip.edu.ph>
2024-08-15 20:05:45 +0800

    Exercise 1.py

--- Exercise 1.py
+++ Exercise 1.py
@@ -9,13 +9,9 @@ class Rectangle:
     def display_area(self):
         print(f"The area of the rectangle is: {self.area()} square units")
 
-
-# Example usage
 length = float(input("Enter the length of the rectangle: "))
 width = float(input("Enter the width of the rectangle: "))
 
-# Creating an instance of Rectangle
 rect = Rectangle(length, width)
 
-# Displaying the area
 rect.display_area()
